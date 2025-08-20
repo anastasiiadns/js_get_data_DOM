2be619a7b48c01a6af70e1b1423a8cab45a242b3
Anastasiia <nastyadenisenko17@gmail.com>
2025-08-20 16:39:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,8 @@ const values = [];
 let sum = 0;
 
 for (const element of elements) {
-  const value = element.textContent;
-  const numberString = value.replace(/[^0-9]/g, '');
+  const valueText = element.textContent;
+  const numberString = valueText.replace(/[^0-9]/g, '');
   const number = parseInt(numberString);
 
   if (!isNaN(number)) {
