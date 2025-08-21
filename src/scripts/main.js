ce1fa6babce8c69301e6129f94b247c1d94da89b
anastasiiadns <nastyadenisenko17@gmail.com>
2025-08-21 17:49:53 +0300

    task fixes 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,22 @@ const elements = document.querySelectorAll('.population');
 const values = [];
 let sum = 0;
 
+let thousandSeparator = '';
+const firstValue = elements[0]?.textContent.trim();
+const match = firstValue?.match(/(\d)([^\d])\d{3}/);
+
+if (match) {
+  thousandSeparator = match[2];
+}
+
 for (const element of elements) {
   const valueText = element.textContent;
-  const numberString = valueText.replace(/[\s,]/g, '');
+  let numberString = valueText;
+
+  if (thousandSeparator) {
+    numberString = valueText.split(thousandSeparator).join('');
+  }
+
   const number = parseInt(numberString);
 
   if (!isNaN(number)) {
@@ -21,8 +34,16 @@ const averageValue = values.length > 0 ? sum / values.length : 0;
 const totalElements = document.querySelectorAll('.total-population');
 const averageElements = document.querySelectorAll('.average-population');
 
-const resultSum = sum.toLocaleString('en-US');
-const resultAverageValue = Math.round(averageValue).toLocaleString('en-US');
+function formatNumber(n, separator) {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+}
+
+const resultSum = formatNumber(sum, thousandSeparator);
+
+const resultAverageValue = formatNumber(
+  Math.round(averageValue),
+  thousandSeparator,
+);
 
 for (const element of totalElements) {
   element.textContent = resultSum;
